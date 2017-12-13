4f1c11796ac551ae20ed3e56504fd3adeadfb642
Chris Thomas <christhomas17@gmail.com>
2017-12-13 16:00:10 -0500

    new

--- 54 - poker.py
+++ 54 - poker.py
@@ -29,7 +29,7 @@ def is_flush(hand):
 		suits.append(get_suit(card))
 
 	return(len(set(suits)) == 1)
-
+#pair, three of a kind etc
 def card_counts(hand):
 	values = []
 	for card in hand:
@@ -43,16 +43,50 @@ def card_counts(hand):
 
 	return(vals)
 
+#straight
+def is_straight(hand):
+	faces = {'J':10, 'Q':11, 'K':12, 'A':13}
+	values = []
+	for card in hand:
+		values.append(get_value(card))
+
+	if len(set(values)) == 5:
+
+		if '2' in values and 'A' in values:
+			faces = {'T':'10','J':'11', 'Q':'12', 'K':'13', 'A':'1'}
+		else:
+			faces = {'T':'10','J':'11', 'Q':'12', 'K':'13', 'A':'14'}
+		print(values)
+		values = [int(faces[card]) if card in faces else int(card) for card in values]
+		print(values)
+		values = sorted(values)
+		print(values)
+		
+		if int(values[0]) + 4 == int(values[-1]):
+			return(True)
+
+	return(False)
+
+def is_royal(hand):
+	
 
 
-three = data.iloc[1,:5]
+
+
+
+
+
+
+three = data.iloc[6,:5]
 print(three)
 
-for i in range(len(data)):
-	hand = data.iloc[i,:5]
-	if is_flush(hand):
-		print(hand)
-		print(i)
+print(is_straight(three))
+
+# for i in range(len(data)):
+# 	hand = data.iloc[i,:5]
+# 	if is_flush(hand):
+# 		print(hand)
+# 		print(i)
 # print(card_counts(hand))
 # print(hand)
 
